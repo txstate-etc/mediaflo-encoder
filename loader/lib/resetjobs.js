d8a236911c7556d601e0177cff568824e373c954
Jeff Snider <jsnider@txstate.edu>
2019-03-04 13:37:13 -0600

    Oops, left the resetjobs interval at my testing rate

--- loader/lib/resetjobs.js
+++ loader/lib/resetjobs.js
@@ -3,4 +3,4 @@ const db = require('txstate-node-utils/lib/mysql')
 setInterval(async () => {
   await db.update("update queue set status='error', error='Reset count exceeded' where status='working' and date_add(encoding_lastupdated, interval 5 minute) < now() and reset_count=2")
   await db.update("update queue set status='waiting', reset_count=reset_count+1 where status='working' and date_add(encoding_lastupdated, interval 5 minute) < now()")
-}, 5000)
+}, 60000)
