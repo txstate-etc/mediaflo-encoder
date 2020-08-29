3b04c8db36673d713898b0f382d08e65c9cbe7c8
Nick Wing <nwing@txstate.edu>
2020-08-29 11:16:56 -0500

    make sure there is a default encoding after we skip an upsize

--- loader/lib/ensembledb.js
+++ loader/lib/ensembledb.js
@@ -1,9 +1,23 @@
-const db = require('txstate-node-utils/lib/mssql')
+const db = require('mssql-async/db')
 
 class Db {
   async dropencodingforjob (jobid) {
-    return db.update('UPDATE Encodings SET IsDeleted=1 FROM Transcoding.TranscodingJobs tj join ServiceJobs sj on tj.ID = sj.ReferenceID join Encodings e on sj.MediaID=e.EncodingID WHERE tj.TranscoderJobId = @jobid', {
-      jobid: jobid
+    return db.transaction(async db => {
+      const { VideoID, EncodingID, IsDefault } = await db.getrow(`
+        SELECT e.VideoID, e.EncodingID, e.IsDefault
+        FROM Transcoding.TranscodingJobs tj
+        JOIN ServiceJobs sj ON tj.ID = sj.ReferenceID
+        JOIN Encodings e ON sj.MediaID=e.EncodingID
+        WHERE tj.TranscoderJobId = @jobid
+      `, { jobid })
+      await db.update('UPDATE Encodings SET IsDefault=0, IsDeleted=1 WHERE EncodingID=@EncodingID', { EncodingID })
+      if (IsDefault) {
+        await db.update(`
+          WITH ENC AS (
+            SELECT TOP 1 * FROM Encodings WHERE VideoID=@VideoID AND NOT IsDeleted ORDER BY Width DESC
+          ) UPDATE ENC SET IsDefault=1
+        `, { VideoID })
+      }
     })
   }
 }
